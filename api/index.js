ccb637ab5162bb2208ac65525d3d39db48d65a8e
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-15 19:42:01 -0300

    chore: update shareCode to 10O1MS7BHFH

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=CMAXY5MHKOU&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=10O1MS7BHFH&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
