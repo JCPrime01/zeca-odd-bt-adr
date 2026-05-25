562c3994062bb3c6e98858d8cbe38a2b2a9e9201
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-25 19:11:08 -0300

    chore: update shareCode to 8TOZH93IX2X

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=2IDEJ2C0FYR&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=8TOZH93IX2X&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
