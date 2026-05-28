706485b564934cce40188d2067b9b2c34945066e
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-28 12:35:15 -0300

    chore: update shareCode to F7CX0PF7UAA

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=U1VW4IYUIAX&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=F7CX0PF7UAA&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
