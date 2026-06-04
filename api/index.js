0aaa095d25f3c86715fc3f324b4fca333f919aa6
JCPrime01 <vanzokiller@gmail.com>
2026-06-04 09:47:13 -0300

    chore: shareCode 8BXYPW5ZH0Y -> 2CPOV8IP94F (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=8BXYPW5ZH0Y&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=2CPOV8IP94F&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
