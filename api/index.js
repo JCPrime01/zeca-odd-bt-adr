1c7b4f747072c68129379d2f493e08fef6d360c3
JCPrime01 <vanzokiller@gmail.com>
2026-06-08 09:49:33 -0300

    chore: shareCode PCKK976MMDI -> QW6IV167RWG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=PCKK976MMDI&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=QW6IV167RWG&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
