61b67d1fbdd6b3b4cf41398fe86d7cc41139c4b2
JCPrime01 <vanzokiller@gmail.com>
2026-06-09 10:42:30 -0300

    chore: shareCode QW6IV167RWG -> HPDYRO8EQYN (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=QW6IV167RWG&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/sg03jy8u?shareCode=HPDYRO8EQYN&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
