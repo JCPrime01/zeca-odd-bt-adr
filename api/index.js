603c6ddbbc2327c4b9fb726f0fa205e533d48c3d
JCPrime01 <vanzokiller@gmail.com>
2026-06-14 11:14:30 -0300

    chore: shareCode HMEMNOQ0ZB6 -> H11EM0FCO30 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=HMEMNOQ0ZB6&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=H11EM0FCO30&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
