54a567290f71f2b675232dfa3d7b741ddab5ebb6
JCPrime01 <vanzokiller@gmail.com>
2026-06-19 10:51:21 -0300

    chore: shareCode ILKQUW0MMWK -> 6MKBH0I9437 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=ILKQUW0MMWK&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=6MKBH0I9437&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
