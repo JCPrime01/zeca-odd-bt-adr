3c4ff3faea461c66264ee1de41ba57ffc52ca989
JCPrime01 <vanzokiller@gmail.com>
2026-06-24 11:53:47 -0300

    chore: shareCode WB4QHPBMNSQ -> VR2ZGUWBS6Q (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=WB4QHPBMNSQ&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=VR2ZGUWBS6Q&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
