934a704e37328ad5b2f7e6c8edd529786e70087c
JCPrime01 <vanzokiller@gmail.com>
2026-07-07 10:16:27 -0300

    chore: shareCode AL2TQ21O3NT -> XC50QURZD5W (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=AL2TQ21O3NT&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=XC50QURZD5W&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
