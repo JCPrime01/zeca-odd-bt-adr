1d996ca4a95a6e19a4fc148cb856d39e7aef4904
JCPrime01 <vanzokiller@gmail.com>
2026-07-12 11:32:42 -0300

    chore: shareCode N48TRADNNZU -> 8EWRB5UF543 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=N48TRADNNZU&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=8EWRB5UF543&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
