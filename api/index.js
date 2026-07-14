f724d030e35a71bf3933e17d6e0c3a298e07e021
JCPrime01 <vanzokiller@gmail.com>
2026-07-14 09:32:13 -0300

    chore: shareCode SUTPDV9HPT7 -> SUODJQ79VXR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=SUTPDV9HPT7&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=SUODJQ79VXR&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
