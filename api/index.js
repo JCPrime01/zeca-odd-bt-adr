6f3ebf0d7778c847d45a953fcfef1240edc6c760
JCPrime01 <vanzokiller@gmail.com>
2026-07-17 10:35:33 -0300

    chore: shareCode 61SG8FKNWW7 -> 1IPWKPGOSBR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=61SG8FKNWW7&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=1IPWKPGOSBR&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
