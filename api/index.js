b3e1fd5f195153cb9439b33b336550d6d5481b6d
JCPrime01 <vanzokiller@gmail.com>
2026-07-19 14:12:32 -0300

    chore: shareCode O2WWCHDTNMS -> ESZG6EK768L (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=O2WWCHDTNMS&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23425&shareCode=ESZG6EK768L&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
